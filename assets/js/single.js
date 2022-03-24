634dd6ad27a90eb535a938b180e5f2b4b4dcc40d
Gerard Mennella <gerardmennella21@gmail.com>
2022-03-24 17:47:58 -0400

    view all issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container")
+var limitWarningEl = document.querySelector("#limit-warning")
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc"
@@ -7,6 +8,10 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data){
                 displayIssues(data)
+
+                if (response.headers.get("Link")) {
+                    displayWarning(repo)
+                }
             })
         } else {
             alert("There was a problem with your request!")
@@ -14,7 +19,23 @@ var getRepoIssues = function(repo) {
     })
 }
 
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit "
+
+    var linkEl = document.createElement("a")
+    linkEl.textContent = "See More Issues on Github.com"
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues")
+    linkEl.setAttribute("target", "_blank")
+
+    limitWarningEl.appendChild(linkEl)
+}
+
 var displayIssues = function(issues) {
+
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!"
+        return
+    }
 
     for (var i = 0; i < issues.length; i++) {
         var issueEl = document.createElement("a")
